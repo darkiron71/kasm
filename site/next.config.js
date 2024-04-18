51d8ee4922a2b65bae3eed5ee0af20dfb29934df
darkiron <130187681+darkiron71@users.noreply.github.com>
2024-04-17 21:05:43 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/darkiron71/kasm/1.0',
+  basePath: '/kasm/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
